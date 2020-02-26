b59989d3f6738f5a9f6ac876cdb648e6ed598200
shaisob kishor dhar <58828304+shaisob@users.noreply.github.com>
2020-02-26 16:55:56 +0600

    mid
    final

---
+++ admin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form>
+	<fieldset style = "width:500px"align="center" >
+		<h4> Welcome Bob!</h4>
+		<a href="page2.html">Profile </a> <br/>
+		<a href="page2.html">Change password </a><br/>
+		<a href="page2.html">View user </a><br/>
+		<a href="page2.html">Log out</a>
+	
+	
+	</fieldset>
+	</form>
+</body>
+</html>
